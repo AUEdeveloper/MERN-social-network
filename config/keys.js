bb6ca865776262e5de53713a86f0b87eada6d640
yan <bryansky.yan2012@yandex.ru>
2019-02-24 17:37:03 +0300

    Initial express server wth route files

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://yan:310197yan@cluster1-shard-00-00-4tdgl.mongodb.net:27017,cluster1-shard-00-01-4tdgl.mongodb.net:27017,cluster1-shard-00-02-4tdgl.mongodb.net:27017/test?ssl=true&replicaSet=Cluster1-shard-0&authSource=admin&retryWrites=true"
+};
