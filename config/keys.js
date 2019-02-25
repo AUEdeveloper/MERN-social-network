975aeb824cae3c0db314d4bbd1a3e7350d607f65
yan <bryansky.yan2012@yandex.ru>
2019-02-25 18:24:57 +0300

    created a JWT token for login process in users.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://yan:310197yan@cluster1-shard-00-00-4tdgl.mongodb.net:27017,cluster1-shard-00-01-4tdgl.mongodb.net:27017,cluster1-shard-00-02-4tdgl.mongodb.net:27017/test?ssl=true&replicaSet=Cluster1-shard-0&authSource=admin&retryWrites=true"
+    "mongodb://yan:310197yan@cluster1-shard-00-00-4tdgl.mongodb.net:27017,cluster1-shard-00-01-4tdgl.mongodb.net:27017,cluster1-shard-00-02-4tdgl.mongodb.net:27017/test?ssl=true&replicaSet=Cluster1-shard-0&authSource=admin&retryWrites=true",
+  secretOrKey: "secret"
 };
