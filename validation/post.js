0fa7f7eb1d223b828447ac80943e7ce6d155e920
yan <bryansky.yan2012@yandex.ru>
2019-03-01 22:21:07 +0300

    added like and unlike routes for posts

--- validation/post.js
+++ validation/post.js
@@ -4,7 +4,7 @@ const isEmpty = require("./is-empty");
 module.exports = function validatePostInput(data) {
   let errors = {};
 
-  //validator can work only with string type, thus it's neccessary to create an empty string if data.name is empty
+  //validator can work only with string type, thus it's neccessary to create an empty string if data.text is empty
   data.text = !isEmpty(data.text) ? data.text : "";
 
   if (!validator.isLength(data.text, { min: 1, max: 300 })) {
